347dd09aafa0718800dbe39175ffadf9f6600e5b
htiger7856-rgb <htiger7856@gmail.com>
2025-11-28 14:51:14 +0500

    Refactor service worker for caching and activation
    Change

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,46 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("billing").then(cache=>{
-      return cache.addAll(["index.html"])
-    })
-  )
-})
+const CACHE_NAME = "m-asad-billing-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  // Add icons if you have them
+  // "./icon-192.png",
+  // "./icon-512.png"
+];
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request)
+// Install service worker and cache files
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching files");
+      return cache.addAll(FILES_TO_CACHE);
     })
-  )
-})
+  );
+  self.skipWaiting();
+});
+
+// Activate service worker and remove old caches
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch cached files when offline
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
